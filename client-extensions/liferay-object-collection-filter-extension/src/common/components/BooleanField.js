aef3fde13362078b739e75ee10f0ff6eb33c42f1
tototrinh <to.trinh@codeenginestudio.com>
2023-12-28 18:26:24 +0700

    Refactor

--- client-extensions/liferay-object-collection-filter-extension/src/common/components/BooleanField.js
+++ client-extensions/liferay-object-collection-filter-extension/src/common/components/BooleanField.js
@@ -2,40 +2,35 @@ import React from 'react';
 import ClayDropdown from "@clayui/drop-down";
 import ClayButton from "@clayui/button";
 import ClayForm, {ClayCheckbox} from "@clayui/form";
-import './BooleanField.scss';
 
 const BooleanField = ({fieldName, fieldType}) => {
 
     return (
-        <>
-            <label>{fieldName}</label>
-            <ClayDropdown
-                className="dropdown-menu-width-shrink"
-                trigger={
-                    <ClayButton
-                        displayType="secondary"
-                        className="bg-light font-weight-normal form-control-select text-left w-100"
+        <ClayDropdown
+            className="dropdown-menu-width-shrink"
+            trigger={
+                <ClayButton
+                    displayType="secondary"
+                    className="bg-light font-weight-normal form-control-select text-left w-100"
 
-                    >
-                        {fieldType}
-                    </ClayButton>
-                }
-            >
-                <>
-                    <ClayForm.Group className="clay-form-group">
-                        <ClayCheckbox
-                            aria-label="True"
-                            label="True"
-                        />
-                        <ClayCheckbox
-                            aria-label="False"
-                            label="False"
-                        />
-                    </ClayForm.Group>
-                </>
-            </ClayDropdown>
-        </>
-
+                >
+                    {fieldType}
+                </ClayButton>
+            }
+        >
+            <>
+                <ClayForm.Group className="clay-form-group">
+                    <ClayCheckbox
+                        aria-label="True"
+                        label="True"
+                    />
+                    <ClayCheckbox
+                        aria-label="False"
+                        label="False"
+                    />
+                </ClayForm.Group>
+            </>
+        </ClayDropdown>
     )
 }
 export default BooleanField
