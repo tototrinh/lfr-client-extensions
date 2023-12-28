6cd8bdd5790c72e1dd33bc16619e1d88fb7eedf2
tototrinh <to.trinh@codeenginestudio.com>
2023-12-28 18:26:24 +0700

    Refactor

--- client-extensions/liferay-object-collection-filter-extension/src/common/components/NumericField.js
+++ client-extensions/liferay-object-collection-filter-extension/src/common/components/NumericField.js
@@ -1,33 +1,28 @@
 import React from 'react';
 import ClayDropdown from "@clayui/drop-down";
 import ClayButton from "@clayui/button";
-import Input from "./Input";
+import NumericInput from "./NumericInput";
 
 const NumericField = ({fieldName, fieldType}) => {
 
     return (
-        <>
-            <label>{fieldName}</label>
-            <ClayDropdown
-                className="dropdown-menu-width-shrink"
-                trigger={
-                    <ClayButton
-                        displayType="secondary"
-                        className="bg-light font-weight-normal form-control-select text-left w-100"
+        <ClayDropdown
+            className="dropdown-menu-width-shrink"
+            trigger={
+                <ClayButton
+                    displayType="secondary"
+                    className="bg-light font-weight-normal form-control-select text-left w-100"
 
-                    >
-                        {fieldType}
-                    </ClayButton>
-                }
-            >
-                <>
-                    <Input name="min" label="Min"/>
-                    <Input name="max" label="Max"/>
-                </>
-            </ClayDropdown>
-        </>
-
-
+                >
+                    {fieldType}
+                </ClayButton>
+            }
+        >
+            <div>
+                <NumericInput name="min" label="Min"/>
+                <NumericInput name="max" label="Max"/>
+            </div>
+        </ClayDropdown>
     )
 }
 export default NumericField
